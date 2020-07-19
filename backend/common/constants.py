62937cda26ba79426b0b92e205baeb5bf0e78530
lws803 <lws803@gmail.com>
2020-07-19 15:02:00 +0800

    added model files

---
+++ backend/common/constants.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class VisibilityType(Enum):
+    PUBLIC = 'public'
+    PRIVATE = 'private'
--- backend/common/models.py
+++ backend/common/models.py
@@ -1,6 +1,17 @@
-from sqlalchemy import BigInteger, Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    BigInteger,
+    Column,
+    DateTime,
+    Enum,
+    ForeignKey,
+    String,
+    func,
+)
+from sqlalchemy.dialects.mysql import JSON
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from common.constants import VisibilityType
 
 
 class MyBase(object):
@@ -10,3 +21,43 @@ class MyBase(object):
 
 
 Base = declarative_base(cls=MyBase)
+
+
+class User(object):
+    __tablename__ = 'users'
+    id = Column(BigInteger, primary_key=True)
+    uid = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+    posts = relationship('Post', backref='user', cascade='all, delete-orphan')
+    owned_groups = relationship('Group', backref='user', cascade='all, delete-orphan')
+
+
+class Post(object):
+    __tablename__ = 'posts'
+    id = Column(BigInteger, primary_key=True)
+    metadata = Column(JSON, nullable=True)
+    tags = Column(JSON, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+    group_id = Column(ForeignKey('groups.id'), nullable=False)
+    owner_id = Column(ForeignKey('users.id'), nullable=False)
+    visibility = Column(Enum(VisibilityType), nullable=False)
+
+
+class Group(object):
+    __tablename__ = 'groups'
+    id = Column(BigInteger, primary_key=True)
+    metadata = Column(JSON, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+    owner_id = Column(ForeignKey('users.id'), nullable=False)
+    admins = Column(JSON, nullable=True)
+
+    posts = relationship('Post', backref='group', cascade='all, delete-orphan')
+
+
+users = User.__table__
+posts = Post.__table__
+groups = Group.__table__
