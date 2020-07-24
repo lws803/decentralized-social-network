5c5b9cc9bad521bc64fc65d8f6b87e0b046d762b
lws803 <lws803@gmail.com>
2020-07-25 00:38:53 +0800

    added consensus client

---
+++ mysql_proxy/proxyServer/helpers/consensus.js
@@ -0,0 +1,75 @@
+var PROTO_PATH = __dirname + "/protos/transaction.proto";
+
+var grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+var transactionProto = grpc.loadPackageDefinition(packageDefinition)
+  .transaction;
+
+class ConsensusClient {
+  static findTrackers(dbSession, callback) {
+    dbSession.query("SELECT * from trackers", (error, results) => {
+      if (error) callback(undefined);
+      callback(results);
+    });
+  }
+
+  static findLatestHash(dbSession, callback) {
+    let query = "SELECT * FROM blockchain ORDER BY id DESC LIMIT 1";
+    dbSession.query(query, (error, results) => {
+      if (error) callback(undefined);
+      if (results) {
+        return callback(results[0].hash);
+      } else {
+        return callback(undefined);
+      }
+    });
+  }
+
+  static sendTransaction(latestHash, payload, url, callback) {
+    var client = new transactionProto.Transaction(
+      `${url}:50051`,
+      grpc.credentials.createInsecure() // TODO: Create secure ssl in future
+    );
+    client.sendBlock(
+      {
+        precedingHash: latestHash,
+        encryptedPayload: payload,
+      },
+      (err, response) => {
+        if (err) callback(false);
+        // TODO: We need to handle what happens when response does not exist
+        callback(response.acknowledgement);
+      }
+    );
+  }
+
+  static sendTransactionToAll(payload, dbSession, callback) {
+    var acceptedCount = 0;
+    this.findLatestHash(dbSession, latestHash => {
+      this.findTrackers(dbSession, trackers => {
+        for (let i = 1; i < trackers.length; i++) {
+          let url = trackers[i].url;
+          // TODO: Consider the active status of the tracker in future
+          this.sendTransaction(latestHash, payload, url, acknowledgement => {
+            if (acknowledgement) acceptedCount += 1;
+          });
+        }
+        // At least 51% consent
+        if (acceptedCount / trackers.length > 0.51) {
+          callback(true);
+        } else {
+          callback(false);
+        }
+      });
+    });
+  }
+}
+
+module.exports = ConsensusClient;
