24fb5b1be403614f3550a4bbed4037e74b15108d
lws803 <lws803@gmail.com>
2020-07-27 00:53:57 +0800

    added proto

---
+++ db/src/protos/packageDefinition.js
@@ -0,0 +1,12 @@
+var PROTO_PATH = __dirname + "/helloworld.proto";
+var protoLoader = require("@grpc/proto-loader");
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+
+module.exports = packageDefinition
