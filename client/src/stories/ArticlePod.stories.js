88ff882fc184ade444df6411d168eccd1e8ce5c7
lws803 <lws803@gmail.com>
2020-08-03 21:31:49 +0800

    added pod list with infinite scrolling in storybooks

--- client/src/stories/ArticlePod.stories.js
+++ client/src/stories/ArticlePod.stories.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { action } from "@storybook/addon-actions";
+import InfiniteScroll from "react-infinite-scroll-component";
+import styled from "styled-components";
+
 import Pod from "../articles/Pod";
-
 import SanFran from "../res/sanfrancisco.jpg";
 
 export default {
@@ -28,3 +29,43 @@ export const Default = () => (
     time="3 Aug, 2020"
   />
 );
+
+export const PodListView = () => {
+  const PodContainer = styled.div`
+    margin-bottom: 40px;
+  `;
+  const [items, setItems] = useState(Array.from({ length: 20 }));
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      setItems(items.concat(Array.from({ length: 20 })));
+    }, 1500);
+  };
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length}
+      next={fetchMoreData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
+      {items.map((i, index) => (
+        <PodContainer>
+          <Pod
+            coverPhoto={SanFran}
+            title="Hello penyet"
+            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Dapibus ultrices in iaculis nunc sed 
+            augue lacus. Quam nulla porttitor massa id neque aliquam. Ultrices mi tempus imperdiet 
+            nulla malesuada. Eros in cursus turpis massa tincidunt dui ut ornare lectus. 
+            Egestas sed sed risus pretium. Lorem dolor sed viverra ipsum. 
+            Gravida rutrum quisque non tellus…
+            "
+            author="lws803"
+            time={`${index} Aug, 2020`}
+          />
+        </PodContainer>
+      ))}
+    </InfiniteScroll>
+  );
+};
