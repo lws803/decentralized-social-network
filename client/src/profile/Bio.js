5f23882fd666fe5cd787563e9028916910d139cf
lws803 <lws803@gmail.com>
2020-08-03 12:26:18 +0800

    bio component

--- client/src/profile/Bio.js
+++ client/src/profile/Bio.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import DOMPurify from "dompurify";
-import styled from "styled-components";
 import draftToHtml from "draftjs-to-html";
 
 class Bio extends React.Component {
