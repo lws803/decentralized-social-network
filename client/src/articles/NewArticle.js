234ebd21b1ee282c86f3d2d81a18a69bf7d0907d
lws803 <lws803@gmail.com>
2020-08-04 23:05:55 +0800

    added some todo

---
+++ client/src/articles/NewArticle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CustomCKEditor from "../common/CustomCKEditor";
+import NavigationBar from "../navBar/NavigationBar";
+
+class NewArticle extends React.Component{
+  render() {
+    return "hello world";
+  }
+}
+
+export default NewArticle;
+// TODO: Design the full page for creating a new article post
