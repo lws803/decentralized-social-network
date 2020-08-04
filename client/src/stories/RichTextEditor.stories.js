7c929ac57eab22df6f979945931ca515d9a9a46b
lws803 <lws803@gmail.com>
2020-08-04 11:14:00 +0800

    added ckeditor custom

--- client/src/stories/RichTextEditor.stories.js
+++ client/src/stories/RichTextEditor.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { action } from "@storybook/addon-actions";
+
 import RichTextEditor from "../common/RichTextEditor";
 
 export default {
@@ -20,3 +21,33 @@ export const Default = () => <RichTextEditor onSave={action("save")} />;
 export const WithContent = () => (
   <RichTextEditor onSave={action("save")} initial={initial} />
 );
+
+// export const NewEditor = () => {
+//   ClassicEditor.create(document.querySelector("#editor"), {
+//     plugins: [SimpleUploadAdapter, CodeBlock],
+//     toolbar: ["codeBlock"],
+//     simpleUpload: {
+//       // Feature configuration.
+//     },
+//   }).then();
+//   return (
+//     <CKEditor
+//       editor={ClassicEditor}
+//       data="<p>Hello from CKEditor 5!</p>"
+//       onInit={editor => {
+//         // You can store the "editor" and use when it is needed.
+//         console.log("Editor is ready to use!", editor);
+//       }}
+//       onChange={(event, editor) => {
+//         const data = editor.getData();
+//         console.log({ event, editor, data });
+//       }}
+//       onBlur={(event, editor) => {
+//         console.log("Blur.", editor);
+//       }}
+//       onFocus={(event, editor) => {
+//         console.log("Focus.", editor);
+//       }}
+//     />
+//   );
+// };
