73781c95f4db9f72412e2423ccbfd2d3ad58c20e
lws803 <lws803@gmail.com>
2020-08-11 22:53:34 +0800

    homepage with big logo

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,34 @@
 import React from "react";
 
-import { PageContainer } from "../common/CommonStyles";
+import styled from "styled-components";
+import ReactFitText from "react-fittext";
+import { Image } from "semantic-ui-react";
 
-class Main extends React.Component {
-  render() {
-    return <PageContainer>Hello world</PageContainer>;
-  }
+import { PageContainer } from "../common/CommonStyles";
+import PenyetImage from "../res/penyet.png";
+
+export default function Main(props) {
+  return (
+    <PageContainer>
+      <LogoContainer>
+        <Image src={PenyetImage} fluid />
+      </LogoContainer>
+      <ReactFitText compressor={6} style={{ width: "100%" }}>
+        <TaglineContainer>Inclusivity and openness</TaglineContainer>
+      </ReactFitText>
+    </PageContainer>
+  );
 }
 
-export default Main;
+const LogoContainer = styled.div`
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+`;
+
+const TaglineContainer = styled.div`
+  margin-top: 5px;
+  font-family: Georgia;
+  text-align: center;
+`;
