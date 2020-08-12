4a2580bec8c09d7ed7a161d04d12992205f7336d
lws803 <lws803@gmail.com>
2020-08-12 14:42:51 +0800

    fixed issue with importing css

--- client/src/stories/0-Welcome.stories.js
+++ client/src/stories/0-Welcome.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
+import "semantic-ui-css/semantic.min.css";
 
 export default {
   title: 'Welcome',
