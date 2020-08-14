281b28af5363c30a1dc74ea6c860b67a8fe11129
lws803 <lws803@gmail.com>
2020-08-14 14:44:27 +0800

    Added new settings page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,10 +1,62 @@
 import React from "react";
 
+import { Accordion, Icon } from "semantic-ui-react";
+
 import { PageContainer } from "../common/CommonStyles";
 
 class Settings extends React.Component {
+  state = { activeIndex: 0 };
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <PageContainer>Settings page</PageContainer>;
+    const { activeIndex } = this.state;
+
+    return (
+      <PageContainer>
+        {" "}
+        <Accordion fluid styled>
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.handleClick}
+          >
+            <Icon name="dropdown" />
+            Reset password
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <p>
+              A dog is a type of domesticated animal. Known for its loyalty and
+              faithfulness, it can be found as a welcome guest in many
+              households across the world.
+            </p>
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.handleClick}
+          >
+            <Icon name="dropdown" />
+            Logout
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            <p>
+              There are many breeds of dogs. Each breed varies in size and
+              temperament. Owners often select a breed of dog that they find to
+              be compatible with their own lifestyle and desires from a
+              companion.
+            </p>
+          </Accordion.Content>
+        </Accordion>
+      </PageContainer>
+    );
   }
 }
 
