99777ae0768996b2b7f1b855118cdaa973f7fbd7
lws803 <lws803@gmail.com>
2020-08-19 21:40:07 +0800

    removed unused lines

--- client/src/profile/ProfileImage.js
+++ client/src/profile/ProfileImage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import styled from "styled-components";
 import LazyImage from "../common/LazyImage";
 
 import Placeholder from "../res/profile_placeholder.png";
