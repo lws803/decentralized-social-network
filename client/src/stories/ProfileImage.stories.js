790227033e18a4020d65132376dbdb6a9f112c03
lws803 <lws803@gmail.com>
2020-08-19 21:39:45 +0800

    removed unused lines

--- client/src/stories/ProfileImage.stories.js
+++ client/src/stories/ProfileImage.stories.js
@@ -1,7 +1,6 @@
 import React from "react";
 import profileImage from "../res/7874219.jpeg";
 
-import { action } from "@storybook/addon-actions";
 import ProfileImage from "../profile/ProfileImage";
 
 export default {
