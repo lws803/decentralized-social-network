e64c969c2a0a72cf6417872fa0bb89999b8ac4a3
lws803 <lws803@gmail.com>
2020-08-23 22:24:18 +0800

    added emotion

--- client/src/articles/Embeddings.js
+++ client/src/articles/Embeddings.js
@@ -1,10 +1,10 @@
-import React from "react";
-
+/** @jsx jsx */
 import SyntaxHighlighter from "react-syntax-highlighter";
 import monokai from "react-syntax-highlighter/dist/esm/styles/hljs/monokai";
 import ReactPlayer from "react-player";
+import { css, jsx } from "@emotion/core";
 
-function transform(node, children) {
+const transform = (node, children) => {
   // Embed code blocks
   if (node.tagName === "PRE" && children.length) {
     if (children[0].props.tagName === "code") {
@@ -25,10 +25,14 @@ function transform(node, children) {
       <ReactPlayer
         url={node.children[0].getAttribute("url")}
         controls
-        style={{ margin: "1em auto", maxWidth: "100%", minWidth: "50px" }}
+        css={css`
+          margin: 1em auto;
+          max-width: 100%;
+          min-width: 50px;
+        `}
       />
     );
   }
-}
+};
 
 export { transform };
