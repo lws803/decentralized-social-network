8e2978e3d1fd7df40b250b970d15d0e2aff7d841
lws803 <lws803@gmail.com>
2020-08-30 16:22:25 +0800

    added large card snapshot test

--- client/src/components/articles/tests/ProfileCard.test.js
+++ client/src/components/articles/tests/ProfileCard.test.js
@@ -1,24 +1,37 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import {createSerializer} from 'enzyme-to-json';
+import { createSerializer } from "enzyme-to-json";
 import moment from "moment";
 
-import {Card, LargeCard} from "../ProfileCard";
+import { Card, LargeCard } from "../ProfileCard";
 import ProfilePhoto from "../../../res/7874219.jpeg";
 
-describe("Card component", () => {
-  Enzyme.configure({ adapter: new Adapter() });
-  expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
 
+describe("Card component", () => {
   it("renders correctly", () => {
     const wrapper = shallow(
-      <Card authorName="lws803" authorPhoto={ProfilePhoto} dateCreated={moment()}/>
+      <Card
+        authorName="lws803"
+        authorPhoto={ProfilePhoto}
+        dateCreated={moment()}
+      />
     );
     expect(wrapper).toMatchSnapshot();
-  })
-})
+  });
+});
 
 describe("Large Card component", () => {
-
-})
+  it("renders correctly", () => {
+    const wrapper = shallow(
+      <LargeCard
+        authorName="lws803"
+        authorPhoto={ProfilePhoto}
+        bio="The quick brown fox"
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
