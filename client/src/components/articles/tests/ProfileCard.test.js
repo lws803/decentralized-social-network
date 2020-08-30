9b33578f90f05353eb95d411cd62b246a1dbbc98
lws803 <lws803@gmail.com>
2020-08-30 16:48:01 +0800

    updated moment date

--- client/src/components/articles/tests/ProfileCard.test.js
+++ client/src/components/articles/tests/ProfileCard.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { createSerializer } from "enzyme-to-json";
 import moment from "moment";
@@ -16,11 +16,25 @@ describe("Card component", () => {
       <Card
         authorName="lws803"
         authorPhoto={ProfilePhoto}
-        dateCreated={moment()}
+        dateCreated={moment("1995-12-25")}
       />
     );
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("profile clicks", () => {
+    const mockCallBack = jest.fn();
+    const wrapper = mount(
+      <Card
+        authorName="lws803"
+        authorPhoto={ProfilePhoto}
+        dateCreated={moment()}
+        onProfileClick={mockCallBack}
+      />
+    );
+    wrapper.find("#author_name").children().at(0).simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  })
 });
 
 describe("Large Card component", () => {
@@ -34,4 +48,18 @@ describe("Large Card component", () => {
     );
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("profile clicks", () => {
+    const mockCallBack = jest.fn();
+    const wrapper = mount(
+      <LargeCard
+      authorName="lws803"
+      authorPhoto={ProfilePhoto}
+      bio="The quick brown fox"
+      onProfileClick={mockCallBack}
+    />
+    )
+    wrapper.find("#author_name").at(0).simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  })
 });
