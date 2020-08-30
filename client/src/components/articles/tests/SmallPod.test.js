a1009f8289b91776a6c7291166a42af7a98ec203
lws803 <lws803@gmail.com>
2020-08-30 16:12:28 +0800

    images render correctly

--- client/src/components/articles/tests/SmallPod.test.js
+++ client/src/components/articles/tests/SmallPod.test.js
@@ -1,24 +1,42 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson, {createSerializer} from 'enzyme-to-json';
 
 import SmallPod from "../SmallPod";
-
-Enzyme.configure({ adapter: new Adapter() });
+import SanFran from "../../../res/sanfrancisco.jpg";
 
 describe("Small Pod component", () => {
+  Enzyme.configure({ adapter: new Adapter() });
+  expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
   it("renders correctly", () => {
     const wrapper = shallow(
       <SmallPod size={{ width: 200 }} title="Some random title" />
-    );  
+    );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders images correctly", () => {
+    const wrapper = shallow(
+      <SmallPod
+        size={{ width: 200 }}
+        title="Some random title"
+        coverPhoto={SanFran}
+      />
+    );
+    expect(wrapper.find({"src": "sanfrancisco.jpg"}).length).toEqual(1);
   });
 
   it("handles clicks correctly", () => {
     const mockCallBack = jest.fn();
     const wrapper = shallow(
-      <SmallPod size={{ width: 200 }} title="Some random title" onClick={mockCallBack}/>
-    );  
+      <SmallPod
+        size={{ width: 200 }}
+        title="Some random title"
+        onClick={mockCallBack}
+      />
+    );
     wrapper.simulate("click");
 
     expect(mockCallBack.mock.calls.length).toEqual(1);
